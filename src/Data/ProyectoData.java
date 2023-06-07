f675d161eae708a39c887c5da4057753099aedf5
leorivero24 <88672669+leorivero24@users.noreply.github.com>
2023-06-07 17:22:09 -0300

    Commit

--- src/Data/ProyectoData.java
+++ src/Data/ProyectoData.java
@@ -1,10 +1,50 @@
 
 package Data;
 
+import Entidad.Proyecto;
+import Entidad.Tarea;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Gabriel
  */
 public class ProyectoData {
+    
+    
+        private Connection con = null;
+
+    public ProyectoData() {
+        con = ConeccionData.getConexion();
+    }
+
+    public void guardarProyecto(Proyecto proyecto) {
+
+        String sql = "INSERT INTO proyecto (nombre, descripcion, fechaInicio,estado) VALUES ( ?, ?, ?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, proyecto.getNombre());
+            ps.setString(2, proyecto.getDescripcion());
+            ps.setDate(3, Date.valueOf(proyecto.getFechaInicio())); // pasar de localdate a date para sql
+            ps.setInt(4, 1);
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                proyecto.setIdProyecto(rs.getInt("idProyecto"));
+                JOptionPane.showMessageDialog(null, "Proyecto cargado exitosamente.");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Proyecto" + ex.getMessage());
+        }
+
+    }
     
 }
